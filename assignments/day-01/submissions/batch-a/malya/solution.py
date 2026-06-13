a5fc3324e452ed423ea2e285d761bb29d01fd09a
malya srivastava <malyasrivastava886@gmail.com>
2026-06-13 23:39:48 +0530

    Added Day 02 solutions

---
+++ assignments/day-01/submissions/batch-a/malya/solution.py
@@ -0,0 +1,34 @@
+#day-01
+
+
+#1 print name,age and college
+print("malya srivastava")
+print(20)
+print("Buddha Institute Of Technology")
+
+#2 variables for name,age,marks and city
+n="malya srivastava"
+age=20
+marks=85
+city="gorakhpur"
+
+#3data types: string, integer, float, and boolean.
+student_name = "Malya"      # String
+student_age = 20            # Integer
+percentage = 85.5           # Float
+is_student = True           #boolean
+
+
+# 4. List of 5 numbers
+numbers = [10, 20, 30, 40, 50]
+print("List:", numbers)
+
+# 5. Dictionary
+student = {}
+
+student["name"] = "Malya Srivastava"
+student["course"] = "Python"
+student["city"] = "Gorakhpur"
+student["batch"] = "A"
+
+print("dict=",student)
