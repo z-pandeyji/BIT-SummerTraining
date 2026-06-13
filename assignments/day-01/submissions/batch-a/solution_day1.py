0385d0a48db1aff26171436df772ae83d28c15c6
priyanshigupta12 <priyanshigupta9889@gmail.com>
2026-06-13 12:26:03 +0530

    day01 assignment completed

---
+++ assignments/day-01/submissions/batch-a/solution_day1.py
@@ -0,0 +1,34 @@
+print("priyanshi gupta")
+print("my age is 20")
+print("my college name is BUDDHA INSTITUTE OF TECHNOLOGY")
+name="Priyanshi Gupta" 
+age=20
+marks=90
+city="gorakhpur"
+print(name)
+print(age)
+print(marks)
+print(city)
+# string datatype
+name="priyanshi gupta"
+print(name)
+print(type(name))
+# integer datatype
+age=20
+print(age)
+print(type(age))
+# float datatype
+marks=90.5
+print(marks)
+print(type(marks))
+# boolean datatype
+is_student=True
+print(is_student)
+print(type(is_student))
+list=[1,2,3,4,5]
+print(list)
+dict={"name":"priyanshi","course":"b.tech","city":"gorakhpur","batch":"A batch"}
+print(dict)
+# my program is to print my name, age, college name, and city
+# learned different types of data types in python
+# i have learned about list and dict and also how to write comments in python
