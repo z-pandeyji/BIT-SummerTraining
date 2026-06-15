888812f11d55198624ddaede4c0fe9f97d15872a
viveksingh79 <viveksingh.gkp2006@gmail.com>
2026-06-15 13:49:07 +0530

    Add Day 1 assignment by viveksingh

---
+++ assignments/day-01/submissions/batch-a/vivek-singh/solution.py
@@ -0,0 +1,47 @@
+#1 Print your name, age, and college name.
+
+Student_Name = "Vivek Singh"
+Student_age = 20
+college_name = "BUDDHA INSTITUTE OF TECHNOLOGY GIDA GORAKHPUR"
+
+print("Name:", Student_Name)
+print("Age:", Student_age)
+print("College Name:", college_name)
+
+
+#2 Create variables for name, age, marks, and city.
+
+marks = float(input("Enter the total marks:"))  # marks should be entered 
+name = "Vivek Singh"
+age = 20
+print("Marks:-",marks)                         # display the marks
+city = "Kushinagar"
+
+
+#3 Use these data types: string, integer, float, and boolean.
+
+string_variable = "Vivek singh"
+integer_variable = 20
+float_variable = 85.5
+boolean_variable = True
+
+
+#4 Create a list of 5 numbers and print the list.
+
+list1 =[55,10,72,10,15]             #list of numbers
+
+print(list1)
+
+
+#5 Create a dictionary with name, course, city, and batch.
+
+student_dict={
+    "name"      : "Vivek Singh",
+    "course"    : "Python",
+    "city"      : "Kushinagar",
+    "batch"     : 2024
+}
+
+#6 Write one comment explaining what your program does.
+
+# This program demonstrates the use of variables to store and display personal information and print them.
