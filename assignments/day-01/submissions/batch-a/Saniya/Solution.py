2297d6b5142e102cb0713a56659c2f2eac25a96d
Saniya Parveen <saniyaparveen0511nov@gmail.com>
2026-06-16 07:26:50 +0530

    Day 1 assignment saniya

---
+++ assignments/day-01/submissions/batch-a/Saniya/Solution.py
@@ -0,0 +1,55 @@
+# Day 1 Assignment
+#============================================================>>>
+# Q.1 .Print your name, age, and college name.
+
+print(r"Name => Saniya Parveen")
+print(r"Age => 20 ")
+print(r"College name => Buddha Institute of Technology ")             ## Printing the name , age and college name of a person
+print("="*50)
+
+#===============================================================>>>>
+# Q.2. Create variables for name, age, marks, and city.
+Name= "Saniya Parveen"
+Age=20
+Marks= "80%"
+City="Gorakhpur"
+
+print(f"My name is => {Name}")
+print(f"My age is => {Age}")
+print(f"Marks are => {Marks}")
+print(f"The name of my city is {City}")                             ## Printing the name, age and college name using variables
+print("="*50)
+
+#==================================================================>>>>
+#Q.3. Use these data types: string, integer, float, and boolean.
+
+name ="Saniya Parveen"                                              ## String
+age = 20                                                            ## Integer
+sem_marks = 76.5                                                    ## Float
+boolean_variable = True                                             ## Boolean
+print("="*50)
+
+#==================================================================>>>>
+#4. Create a list of 5 numbers and print the list.
+numbers=[10,20,30,40,50]
+print("The numbers in the list are:")
+print(numbers)                                                      # Printing the numbers 
+print("="*50)
+
+#==================================================================>>>>>
+#Q.5. Create a dictionary with name, course, city, and batch.
+
+Student_info={"name":"Saniya Parveen",
+              "course":"Data science",
+              "city":"Gorakhpur",
+              "batch":"A"}
+print("Name :", Student_info["name"])
+print("Course :", Student_info["course"])
+print("City :" ,Student_info["city"])
+print("Batch :", Student_info["batch"])
+print("="*50)                                                       ## using Dictionary Data type
+
+#==================================================================>>>>>>
+#Q.6. Write one comment explaining what your program does.
+
+## Demonstrating the use of variales, data type,lists,and dictionaries in python
