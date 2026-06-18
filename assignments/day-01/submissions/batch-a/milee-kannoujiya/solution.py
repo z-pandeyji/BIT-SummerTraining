7d99df9ab04d1cb0702f6796f58ab3ccaed665f9
kannaujiyamilee <kannaujiyamilee@gmail.com>
2026-06-18 13:30:54 +0530

    Add Day 1 solution

---
+++ assignments/day-01/submissions/batch-a/milee-kannoujiya/solution.py
@@ -0,0 +1,62 @@
+# 1. Print your name, age, and college name.
+
+student_name=input("Enter the name of student:")
+student_age=input("Enter the age of student:")
+college_name=input("Enter the name of college:")
+
+print("student name:",student_name)
+print("student age:",student_age)
+print("college name:",college_name)
+
+
+# 2. Create variables for name, age, marks, and city.
+
+name="Kritika"
+age=20
+marks=86
+city="Gorakhpur"
+
+print(name)
+print(age)
+print(marks)
+print(city)
+
+
+# 3. Use these data types: string, integer, float, and boolean
+
+name = "Kritika"
+city = "Gorakhpur"
+
+age = 20
+
+marks = 86
+
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Marks:", marks)
+print("City:", city)
+print("Is Student:", is_student)
+
+
+# 4. Create a list of 5 numbers and print the list.
+
+
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers)
+
+
+# 5. Create a dictionary with name, course, city, and batch.
+    
+
+student = {
+    "name": "Kritika",
+    "course": "Python Programming",
+    "city": "Gorakhpur",
+    "batch": "2026"
+}
+
+print("Student Details:")
+print(student)
