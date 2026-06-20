480f196e5cefc37f4ee2cc33347a7eabe4fcbacf
Anand Singh <anandsingh1007567@gmail.com>
2026-06-20 12:08:54 +0530

    Day 1 assignment by AnandSingh

---
+++ assignments/day-01/submissions/batch-b/AanadSingh/solution.py
@@ -0,0 +1,35 @@
+# 1. print your name,age,and college name
+print("your name:anand singh")
+print("age: 24")
+print("college name: Buddha Institute of Technology")
+
+# 2. create variables
+name = input("enter your name:")
+age = input("enter your age:")
+marks = input("enter your marks:")
+city = input("enter your city:")
+
+# 3. data tyes
+name = "anand singh"
+age = 24
+marks = 90.45
+is_student=True
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(is_student))
+
+# 4. list of 5 numbers
+nums=[10,20,30,40,50]
+print("list:",nums)
+
+# 5.dictionary
+student={
+    "name": "anand singh",
+    "course": "MCA",
+    "city": "Gorakhpur",
+    "batch": "B"
+}
+print("dictionary",student)
+
+
