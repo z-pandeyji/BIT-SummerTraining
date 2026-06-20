1c699116f2f0d8cd5b52f61751eedae283e2c10f
raj7126770-star <raj7126770@gmail.com>
2026-06-20 21:02:44 +0530

    Update day 03 solution for Raj Kumar

--- assignments/day-03/submissions/batch-b/Raj-kumar/solution.py
+++ assignments/day-03/submissions/batch-b/Raj-kumar/solution.py
@@ -1,21 +1,21 @@
-# # 1. Create a list of 5 student names and print all names.
-# students = ["Rahul", "Deepak", "Alam", "Ansh", "Aradhana"]
-# print("All students:", students)
+# 1. Create a list of 5 student names and print all names.
+students = ["Rahul", "Deepak", "Alam", "Ansh", "Aradhana"]
+print("All students:", students)
 
-# # 2. Add one new student name to the list.
-# students.append("Raj")
-# print("Updated student list:", students)
+# 2. Add one new student name to the list.
+students.append("Raj")
+print("Updated student list:", students)
 
-# # 3. Create a tuple of 5 city names and print the second city.
-# cities = ("Gorakhpur", "Lucknow", "Delhi", "Mumbai", "Patna")
-# print("Second city:", cities[1]) 
+# 3. Create a tuple of 5 city names and print the second city.
+cities = ("Gorakhpur", "Lucknow", "Delhi", "Mumbai", "Patna")
+print("Second city:", cities[1]) 
 
-# # 4. Create a set of 5 course names and add one new course.
-# courses = {"Python", "Data Science", "Machine Learning", "Web Development", "Cyber Security"}
-# courses.add("Cloud Computing")
-# print("Updated courses set:", courses)
+# 4. Create a set of 5 course names and add one new course.
+courses = {"Python", "Data Science", "Machine Learning", "Web Development", "Cyber Security"}
+courses.add("Cloud Computing")
+print("Updated courses set:", courses)
 
-# 5. Create a dictionary for one student with name, branch, batch, and marks.
+5. #Create a dictionary for one student with name, branch, batch, and marks.
 student_info = {
     "name": "Raj Kumar",
     "branch": "IT",
