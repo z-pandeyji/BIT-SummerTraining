61648b0dc5fb468a54a8e6bdba2300fc39b1529c
Rahul Singh <tumhari-github-email@gmail.com>
2026-06-26 16:41:52 +0530

    Add Day 01 Solution1

---
+++ assignments/day-01/submissions/batch-b/Rahul_singh/Solution1.py
@@ -0,0 +1,37 @@
+# Question 1. Print your name, age, and college name.
+
+print("Name: Rahul Singh")
+print("Age: 19")
+print("College: Buddha Institute of Management")
+
+# Question 2. Create variables for name, age, marks, and city.
+
+name = "Rahul Singh"
+age = 19
+marks = 87.5
+city = "Gorakhpur"
+
+# Question 3. Use these data types: string, integer, float, and boolean.
+
+student_name = "Rahul Singh"    #string
+student_age = 19                #integer
+percentage = 87.5               #float
+is_student = True               #boolean
+
+# Question 4. Create a list of 5 numbers and print the list.
+
+numbers = [100, 200, 300, 400, 500]
+print(numbers)
+
+# Question 5. Create a dictionary with name, course, city, and batch.
+
+student_name = {
+    "name": "Rahul Singh",
+    "course": "BCA",
+    "city": "Gorakhpur",
+    "batch": "B"
+}
+print(student_name)
+
+# Question 6. Write one comment explaining what your program does.
+#             This program is my day one python assignment for BIT summer training. 
