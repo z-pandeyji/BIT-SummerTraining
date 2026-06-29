8d23ea8bd7d79d3d3474eed32bfb0af16a810eec
Siddharth <siddharthpanday1234567@gmail.com>
2026-06-30 01:16:22 +0530

    Add Day 1 assignment by Siddharth

---
+++ assignments/day-01/submissions/batch-b/siddharth/solution.py
@@ -0,0 +1,30 @@
+name = "Siddharth"
+age = 20
+marks = 85.5
+city = "Jaunpur"
+college = "BIT"
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+
+my_string = "Hello"
+my_integer = 20
+my_float = 85.5
+my_boolean = True
+
+print(type(my_string))
+print(type(my_integer))
+print(type(my_float))
+print(type(my_boolean))
+
+numbers = [10, 20, 30, 40, 50]
+print("List of numbers:", numbers)
+
+student = {
+    "name": "Siddharth",
+    "course": "BCA",
+    "city": "Gorakhpur",
+    "batch": "B"
+}
+print("Student Info:", student)
