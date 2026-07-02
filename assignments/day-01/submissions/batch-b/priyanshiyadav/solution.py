8efb93139ccb230ee72a889c7940cd7a0774fc65
priyanshi yadav <priyanshiyadav0712@gmail.com>
2026-07-02 09:26:41 +0530

    Update solution.py

--- assignments/day-01/submissions/batch-b/priyanshiyadav/solution.py
+++ assignments/day-01/submissions/batch-b/priyanshiyadav/solution.py
@@ -1,1 +1,32 @@
-i
+# Question 1 
+print("Name: Priyanshi Yadav")
+print("Age: 19")
+print("College: Buddha Degree college, GIDA, Gorakhpur")
+
+# Question 2
+name = "Priyanshi Yadav"
+age = 19
+marks = 85.5
+city = "Gorakhpur"
+
+# Question 3
+student_name = "Priyanshi Yadav"    #string
+student_age = 19                #integer
+percentage = 85.5               #float
+is_student = True               #boolean
+
+# Question 4
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# Question 5
+student_name = {
+    "name": "Priyanshi Yadav",
+    "course": "B.com 2nd year",
+    "city": "Gorakhpur",
+    "batch": "b"
+}
+print(student_name)
+
+# Question 6
+#this program is my day 1 python assignment for BIT summer training. 
