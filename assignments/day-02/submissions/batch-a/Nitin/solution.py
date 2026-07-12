875209d553ce42a0f316b3bd9bfc1f8dff13e936
Nitin Jaiswal <nitinjaiswal104@gmail.com>
2026-07-12 13:26:55 +0530

    Fix Day 3 submission folder

--- assignments/day-02/submissions/batch-a/Nitin/solution.py
+++ assignments/day-02/submissions/batch-a/Nitin/solution.py
@@ -1,63 +1,58 @@
+# Question 1: Check whether a number is positive, negative, or zero
 
-# 1. Check whether a number is positive, negative, or zero
+num = int(input("Enter a number: "))
 
-number = float(input("Enter a number: "))
-
-if number > 0:
-    print("The number is Positive.")
-elif number < 0:
-    print("The number is Negative.")
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
 else:
-    print("The number is Zero.")
+    print("Zero")
 
 
-# 2. Check whether a number is even or odd
+# Question 2: Check whether a number is even or odd
 
-num = int(input("\nEnter an integer: "))
+num = int(input("Enter another number: "))
 
 if num % 2 == 0:
-    print("The number is Even.")
+    print("Even")
 else:
-    print("The number is Odd.")
+    print("Odd")
 
 
-# 3. Create a list of 10 numbers and print each number using a loop
+# Question 3: Create a list of 10 numbers and print each number using a loop
 
-numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print("\nNumbers in the list:")
 for number in numbers:
     print(number)
 
 
-# 4. Function to calculate average of 3 marks
+# Question 4: Function to calculate average of 3 marks
 
 def calculate_average(mark1, mark2, mark3):
     average = (mark1 + mark2 + mark3) / 3
     return average
 
 
-# Example
 avg = calculate_average(85, 90, 95)
 print("\nAverage Marks:", avg)
 
 
-# 5. Function to print grade based on marks
+# Question 5: Function to print grade based on marks
 
 def grade_student(marks):
     if marks >= 90:
-        print("Grade: A+")
-    elif marks >= 80:
         print("Grade: A")
-    elif marks >= 70:
+    elif marks >= 75:
         print("Grade: B")
     elif marks >= 60:
         print("Grade: C")
-    elif marks >= 50:
+    elif marks >= 40:
         print("Grade: D")
     else:
         print("Grade: F")
 
 
-# Example
 grade_student(avg)
